ce433cdca2cb794a5248ba53ca5e964026132690
Chase Dunagan <cdunagan@utexas.edu>
2017-02-27 11:27:25 -0700

    add sad path for signup test

--- spec/features/user_can_create_account_spec.rb
+++ spec/features/user_can_create_account_spec.rb
@@ -15,4 +15,19 @@ RSpec.describe "user can sign up" do
     expect(current_path).to eq(root_path)
     expect(User.all.first.email).to eq("cdun@gmail.com")
   end
+
+  scenario "User cannot sign up if passwords dont match" do
+    visit "/"
+    click_on "Sign-up"
+
+    expect(current_path).to eq(signup_path)
+
+    fill_in "user_email", :with => "chase@gmail.com"
+    fill_in "user_password", :with => "texas"
+    fill_in "user_password_confirmation", :with => "colorado"
+    click_on "Submit"
+
+    expect(current_path).to eq(signup_path)
+    expect(page).to have_content("Your passwords do not match")
+  end
 end
