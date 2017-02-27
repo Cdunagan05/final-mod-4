f8f52eb806c71606bdfcdff28b43e75643639c16
Chase Dunagan <cdunagan@utexas.edu>
2017-02-27 12:51:19 -0700

    add test for invalid url links

--- spec/features/create_link_spec.rb
+++ spec/features/create_link_spec.rb
@@ -13,4 +13,17 @@ RSpec.describe "can create links", :js => :true do
       expect(page).to have_text("http://turing.io")
     end
   end
+
+  scenario "Cannot create link with invalid URL" do
+    stub_logged_in_user
+
+    expect(User.first.links.count).to eq(0)
+
+    fill_in "Title:", :with => "Turing"
+    fill_in "URL:", :with => "ht.turing.io"
+    click_on "Add Link"
+
+    expect(page).to have_content("Please enter a valid URL")
+    expect(User.first.links.count).to eq(0)
+  end
 end
