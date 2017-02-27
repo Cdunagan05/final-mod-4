ba8ae1cedd454c9d3f62a19587e3dca684c9d03a
Chase Dunagan <cdunagan@utexas.edu>
2017-02-27 12:22:42 -0700

    add logout test and made it pass

--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -15,4 +15,22 @@ RSpec.describe "user can login" do
     expect(current_path).to eq(root_path)
     expect(User.all.first.email).to eq("cdunagan@gmail.com")
   end
+
+  scenario "User logs out" do
+    user = User.create(email: "cdunagan@gmail.com", password_digest: "texas")
+    visit "/"
+    click_on "Login"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in "email", :with => "cdunagan@gmail.com"
+    fill_in "password", :with => "texas"
+    click_on "Submit"
+
+    expect(current_path).to eq(root_path)
+
+    click_on "Logout"
+
+    expect(current_path).to eq(login_path)
+  end
 end
