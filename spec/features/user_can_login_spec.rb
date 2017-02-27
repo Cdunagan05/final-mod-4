2de8e068422432d151e9096e6251ef912221908d
Chase Dunagan <cdunagan@utexas.edu>
2017-02-27 12:03:00 -0700

    add login test

---
+++ spec/features/user_can_login_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe "user can login" do
+  scenario "User logs in" do
+    user = User.create(email: "cdunagan@gmail.com", password_digest: "texas")
+    visit "/"
+    click_on "Login"
+
+    expect(current_path).to eq(login_path)
+
+    fill_in "email", :with => "cdunagan@gmail.com"
+    fill_in "password", :with => "texas"
+    click_on "Submit"
+
+    expect(current_path).to eq(root_path)
+    expect(User.all.first.email).to eq("cdunagan@gmail.com")
+  end
+end
