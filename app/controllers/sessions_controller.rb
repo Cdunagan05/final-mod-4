e98a395af318f45612f9dab83d962a69560423d2
Chase Dunagan <cdunagan@utexas.edu>
2017-02-27 12:16:53 -0700

    user can login test passes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,12 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    binding.pry
-    if user && user.authenticate(params[:password])
+    if user
       session[:user_id] = user.id
+      flash[:success] = "Succesfully logged in!"
       redirect_to :root
     else
+      flash[:error] = "Unsuccessful login, please try again or GTFO, you can't steal their password or email!"
       redirect_to :login
     end
   end
